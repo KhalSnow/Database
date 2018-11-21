efec7def2be34017795d1026a031454c30bd10a3
Snow <771314194@qq.com>
2018-11-21 14:04:00 +0800

    third change

--- src/pages/database/database.vue
+++ src/pages/database/database.vue
@@ -19,22 +19,5 @@ export default {
 </script>
 
 <style>
-#tb{
-  width:800px;
-  border-collapse: collapse;
-  margin: 20px auto;
-}
-#tb th{
-  background-color: #0094ff;
-  color: white;
-  font-size: 16px;
-  padding: 5px;
-  text-align: center;
-  border: 1px solid black;
-}
-#tb td{
-  padding: 5px;
-  text-align: center;
-  border: 1px solid black;
-}
+
 </style>
